c9561f3ac63c2d5e31e5adca1a96bf1afffa52f4
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-27 18:03:49 -0300

    Horizontal Screen Tutorial
    3. Project 2 - Horizontal tutorial screen for App

--- rndesign/src/components/Horizontal.js
+++ rndesign/src/components/Horizontal.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { 
     Text,
     View,
+    Dimensions,
     ScrollView,
     StyleSheet,
 } from 'react-native'
@@ -9,9 +10,18 @@ import {
 export default class Horizontal extends Component {
     render() {
         return (
-            <ScrollView style={styles.container}>
+            <ScrollView 
+            horizontal={true} 
+            pagingEnabled={true} 
+            style={styles.container}>
                <View style={styles.outer}>
                    <Text style={styles.innerText}>Welcome to LCO app</Text>
+               </View>
+               <View style={[styles.outer, styles.red]}>
+                   <Text style={styles.innerText}>Best quality videos</Text>
+               </View>
+               <View style={[styles.outer, styles.green]}>
+                   <Text style={styles.innerText}>Available on ios and Android</Text>
                </View>
             </ScrollView>
         )
@@ -23,9 +33,24 @@ const styles = StyleSheet.create({
 
     },
     outer: {
-
-    },
+        backgroundColor: '#007bb6',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+/*         com o dimensions podemos pegar o tamanho da tela
+ */        
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+        },
     innerText: {
-
+        color: '#fff',
+        fontSize: 23,
+        fontWeight: 'bold',
     },
+    red: {
+        backgroundColor: '#dd4b39',
+        },
+    green: {
+        backgroundColor: '#27ae60',
+        },
 });
