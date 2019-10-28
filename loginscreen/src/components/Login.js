72e6470e0f2efcbd32ecde1122be42968d27c8bd
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-28 11:13:59 -0300

    Component Form Criado
    Criando e importando componente form, ajustando alguns posicionamentos de elementos na tela

--- loginscreen/src/components/Login.js
+++ loginscreen/src/components/Login.js
@@ -6,6 +6,7 @@ import {
     Dimensions,
     StyleSheet
 } from 'react-native'
+import Myform from './MyForm';
 
 export default class Login extends Component {
     render() {
@@ -16,8 +17,11 @@ export default class Login extends Component {
                     style={styles.logo}
                         source={require('./images/falk.png')}
                     />
+                    <Text style={styles.title}>A simple login app</Text>
                 </View>
-                
+                <View style={styles.myform}>
+                    <Myform/>
+                </View>
             </View>
         )
     }
@@ -37,5 +41,11 @@ const styles = StyleSheet.create({
     logo:{
         width: 100,
         height: 100,
+    },
+    title:{
+        color: '#fff'
+    },
+    myform:{
+        flex: 1,
     },
 });
