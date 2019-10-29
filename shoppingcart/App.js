ecee9e86228fd80a0fd62226781a7ac439623fd7
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-29 15:36:16 -0300

    Shopping Cart Project
    Criando o shopping cart project e adicionando navbar com imagens

---
+++ shoppingcart/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Header from './app/components/Header';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex : 1,
+    backgroundColor : '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
